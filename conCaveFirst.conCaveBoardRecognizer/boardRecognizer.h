beb2dde89fe9bf30ad70f2890e7e75e806ac91ba
leejh10003 <leejh10003@gmail.com>
2016-03-20 14:31:02 +0900

    Side line check first implementing

--- conCaveFirst.conCaveBoardRecognizer/boardRecognizer.h
+++ conCaveFirst.conCaveBoardRecognizer/boardRecognizer.h
@@ -10,45 +10,13 @@ public:
 		Board::Coordinate start;
 		Board::Coordinate end;
 	};
-	void addLines(Board inputBoard, Board::Coordinate inputStoneCoordinate)
-	{
-		BoardRecognizer::CheckSideExistence::up(inputBoard, inputStoneCoordinate) == true ?
-	}
 private:
 	list<BoardRecognizer::Line> lines;
-	class CheckSideExistence {
-	public:
-		static bool up(Board inputBoard, Board::Coordinate inputStoneCoordinate)
+	bool isThereUpsideLine(Board::Coordinate inputPosition)
+	{
+		for (list<BoardRecognizer::Line>::reverse_iterator searchIterator = this->lines.rbegin(); searchIterator != lines.rend(); searchIterator++)
 		{
-			if (inputStoneCoordinate.y > 0)
-				return true;
-			else
-				return false;
+			if( searchIterator->end.x == inputPosition.x && searchIterator->end.y == inputPosition.y )
 		}
-		static bool down(Board inputBoard, Board::Coordinate inputStoneCoordinate)
-		{
-			if (inputStoneCoordinate.y < inputBoard.getBoardSize().y)
-				return true;
-			else
-				return false;
-		}
-		static bool left(Board inputBoard, Board::Coordinate inputStoneCoordinate)
-		{
-			if (inputStoneCoordinate.x > 0)
-				return true;
-			else
-				return false;
-		}
-		static bool right(Board inputBoard, Board::Coordinate inputStoneCoordinate)
-		{
-			if (inputStoneCoordinate.x < inputBoard.getBoardSize().x)
-				return true;
-			else
-				return false;
-		}
-	};
-	class LineSideCheck {
-	public:
-		static void seek(Board inputBoard, Board::Coordinate inputStoneCoordinatem, BoardRecognizer::Line lines)
-	};
+	}
 };
