a900a58f20c83c154311ae53d10235e76afe6fae
leejh10003 <leejh10003@gmail.com>
2016-03-22 01:11:00 +0900

    for Submission initialized

---
+++ concaveFirst.for3concaveSolver/3solver.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+#include "../conCaveFirst/board.h"
+class solver{
+public:
+
+private:
+	struct pointState {
+		bool isEmergence;
+		int possibleLine;
+	};
+	void findingEmergence(board)
+};
